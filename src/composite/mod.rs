2a9c7d02965b258eaec6b0bee77473f7fb1102cc
guanz42 <guanz42@163.com>
2023-05-31 17:50:38 +0800

    add composite

---
+++ src/composite/mod.rs
@@ -0,0 +1,22 @@
+mod fs;
+
+#[cfg(test)]
+mod tests {
+    use crate::composite::fs::{Component, File, Folder};
+
+    #[test]
+    fn it_works() {
+        let file1 = File::new("File 1");
+        let file2 = File::new("File 2");
+        let file3 = File::new("File 3");
+
+        let mut folder1 = Folder::new("Folder 1");
+        folder1.add(file1);
+
+        let mut folder2 = Folder::new("Folder 2");
+        folder2.add(file2);
+        folder2.add(file3);
+
+        folder2.search("rose");
+    }
+}
