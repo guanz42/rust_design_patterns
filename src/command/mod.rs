b8527566d0929084953866bcf9fd3bb541912b9b
guanz42 <guanz42@163.com>
2023-06-08 17:20:46 +0800

    update command

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,60 +1,17 @@
 #![allow(dead_code)]
 
-mod commands;
-
-use commands::Command;
-use cursive::Cursive;
-
-#[derive(Default)]
-struct AppContext {
-    clipboard: String,
-    history: Vec<Box<dyn Command>>,
-}
-
-fn execute(app: &mut Cursive, mut command: impl Command + 'static) {
-    if command.execute(app) {
-        app.with_user_data(|context: &mut AppContext| {
-            context.history.push(Box::new(command));
-        });
-    }
-}
-
-fn undo(app: &mut Cursive) {
-    let mut context = app.take_user_data::<AppContext>().unwrap();
-    if let Some(mut command) = context.history.pop() {
-        command.undo(app);
-    }
-    app.set_user_data(context);
-}
+mod cohort;
 
 #[cfg(test)]
 mod tests {
-    use cursive::{
-        view::Nameable,
-        views::{Dialog, EditView},
-    };
-
-    use super::{
-        commands::{CopyCommand, CutCommand, PasteCommand},
-        execute, undo, AppContext,
-    };
+    use super::cohort::{AttackCommand, Cohort, CohortCommander, RetreatCommand};
 
     #[test]
-    #[ignore]
     fn it_works() {
-        let mut app = cursive::default();
-
-        app.set_user_data(AppContext::default());
-        app.add_layer(
-            Dialog::around(EditView::default().with_name("Editor"))
-                .title("Type and use bottons")
-                .button("Copy", |s| execute(s, CopyCommand::default()))
-                .button("Cut", |s| execute(s, CutCommand::default()))
-                .button("Paste", |s| execute(s, PasteCommand::default()))
-                .button("Undo", undo)
-                .button("Quit", |s| s.quit()),
-        );
-
-        app.run();
+        let mut commander = CohortCommander::new(Cohort {}, AttackCommand {}, RetreatCommand {});
+        commander.attack();
+        commander.stop();
+        commander.retreat();
+        commander.stop();
     }
 }
